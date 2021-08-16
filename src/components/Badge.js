9fd3d5e660f6d1e854108a8c6db4c43dd1db0964
AlbaIntelligence <Alba.Intelligence@gmail.com>
2021-08-16 14:51:14 +0200

    Commit 2

--- src/components/Badge.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Badge({tag, key}) {
-	return (<div className="badge" key={key}>{tag}</div>);
-}
--- src/demos/building-a-geospatial-app/1-starting-with-map/src/deckgl-layers.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-
-const PICKUP_COLOR = [114, 19, 108];
-const DROPOFF_COLOR = [243, 185, 72];
-
-export function renderLayers(props) {
-  return [];
-}
